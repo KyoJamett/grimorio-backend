c76f9f556d10379143ad0193812ec36002fc2491
KyoJamett <kyo.akamatsu@gmail.com>
2026-04-15 16:00:54 -0400

    feat: allow external connections and update CORS config for local network testing

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 import app from './src/app.js';
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`));
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import formatsRouter from './routes/formats.js'
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', editionsRouter);
